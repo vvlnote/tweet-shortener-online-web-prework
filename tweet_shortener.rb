278e45ef4baf1774e857e3dc0eeaf37e323809ac
Joy Lin <vvlnote@hotmail.com>
2019-03-04 05:15:25 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+
+def dictionary(str)
+  dictionary_hash = {
+    "hello" =>"hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+  
+end
+
+def word_substituter (tweet_str)
+end
